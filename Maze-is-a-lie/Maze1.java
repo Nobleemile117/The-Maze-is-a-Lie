da78e8cd6c13479ef7ae75577858e732dcf69b2d
IsaacCov <IsaacCov@users.noreply.github.com>
2020-05-22 21:31:14 -0500

    Error con archivo

--- Maze-is-a-lie/Maze1.java
+++ Maze-is-a-lie/Maze1.java
@@ -1,29 +1,77 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class Maze1 here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Maze1 extends World
-{
-    private GreenfootSound theme2 =  new  GreenfootSound("copiedCityNier.mp3");
-    private Chell chell =  new  Chell();
-    private int direccion;
-    /**
-     * Constructor for objects of class Maze1.
-     * 
-     */
-    public Maze1()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(800, 800, 1);
-        addObject(chell, 400, 400);
-    }
-    public void act(){
-    theme2.play();
+    import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+    import java.io.*;
     
+    /**
+     * Write a description of class Maze1 here.
+     * 
+     * @author (your name) 
+     * @version (a version number or a date)
+     */
+    public class Maze1 extends World
+    {
+        private GreenfootSound theme2 =  new  GreenfootSound("copiedCityNier.mp3");
+        private Chell chell =  new  Chell();
+        private int direccion;
+        
+        /**
+         * Constructor for objects of class Maze1.
+         * 
+         */
+        public Maze1()
+        {    
+            // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+            super(800, 600, 1);
+            createMaze();
+            addObject(chell, 400, 300);
+        }
+        
+        public void act(){
+        theme2.play();
+        
+        }
+        
+        public void createMaze(){
+            File file = new File("Maze1.txt");
+            
+            FileReader fileR = null;
+            BufferedReader file2 = null;
+            int punterox=0;
+            int punteroy=0;
+            
+            try {
+                fileR = new FileReader(file);
+                file2 = new BufferedReader(fileR);
+                
+            }catch (FileNotFoundException e) {
+                System.out.println("No se encontro el archivo "+file.getName());
+            }
+            
+            try{
+                while(file2.readLine() != null)
+                {
+                    int line = file2.read();
+                    switch(line){
+                        case 1:
+                            addObject(new esquinasuperiorIzquierda(),punterox,punteroy);
+                            punterox+=35;
+                            break;
+                        
+                        case 2:
+                            addObject(new paredhorizontalSuperior(),punterox,punteroy);
+                            punterox+=35;
+                            break;
+                        
+                        case 3:
+                            addObject(new esquinasuperiorderecha(),punterox,punteroy);
+                            punterox=0;
+                            punteroy+=35;
+                            break;
+                    }
+                    System.out.println(line);
+                }
+            }catch(IOException e){
+                    e.printStackTrace();
+            }
     }
    
 }
--- Maze-is-a-lie/paredhorizontalSuperior.java
+++ Maze-is-a-lie/paredhorizontalSuperior.java
@@ -6,11 +6,11 @@ import greenfoot.*;
 /**
  * 
  */
-public class esquinasuperiorIzquierda extends Actor
+public class paredhorizontalSuperior extends Actor
 {
 
     /**
-     * Act - do whatever the esquinasuperiorIzquierda wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     * Act - do whatever the paredhorizontalSuperior wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
