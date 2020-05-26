aa9d69e8af85af0923263b45bbcd086dd48f6712
German Gonzalez <nobleemile117@yahoo.com>
2020-05-26 18:55:53 -0500

    Leaderboard in progress

--- Maze-is-a-lie/TopTime.java
+++ Maze-is-a-lie/TopTime.java
@@ -1,32 +1,33 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.io.BufferedReader;
 import java.io.FileReader;
-import javax.swing.JOptionPane;
 /**
- * Write a description of class ScoreScreen here.
+ * Write a description of class TopTime here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class ScoreScreen extends World
+public class TopTime extends Actor
 {
-    private GreenfootSound theme =  new  GreenfootSound("CaraMia8bit.mp3");
-    private String name;
+     private String highScore = "";
+     private static TopTime uniqueInstance;
     /**
-     * Constructor for objects of class ScoreScreen.
-     * 
+     * Act - do whatever the TopTime wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public ScoreScreen()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(800, 400, 1);
-        
-        
+    public void act() 
+    {
+       if(highScore.equals("")){
+        highScore = this.GetHighScoreValue();
+        }
        
     }
-    public void act(){
-    theme.play();
-    }/*
+    public static TopTime getInstance(){
+        if(uniqueInstance == null){
+            uniqueInstance = new TopTime();
+        }
+        return uniqueInstance;
+    }
     public void DrawHighScore(){
     GreenfootImage img = new GreenfootImage(50, 20);
         img.setColor(Color.BLACK);
@@ -53,11 +54,12 @@ public class ScoreScreen extends World
                 e.printStackTrace();
             }
         }
-    }*/
-    public void compareScores(){
-        if(Stopwatch.getInstance().getTime()<TopTime.getInstance().getTopTime()){
-         String name =JOptionPane.showInputDialog("You set a new. Cual es tu nombre?");
-         
-        }
-        }
-}
+    }
+    public int getTopTime(){
+    return Integer.parseInt((highScore.split(":")[1]));
+    }
+    public void setTopTime(String name, int score){
+        this.highScore = name + ":" + score;
+    }
+    }    
+
