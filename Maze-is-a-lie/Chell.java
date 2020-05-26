97f258b15cb460167089ff842b2cef8985119ad8
IsaacCov <IsaacCov@users.noreply.github.com>
2020-05-25 22:20:25 -0500

    Iniciado el mapa

--- Maze-is-a-lie/Chell.java
+++ Maze-is-a-lie/Chell.java
@@ -7,59 +7,59 @@ import greenfoot.*;
  */
 public class Chell extends Actor
 {
-     private int direccion;
+    private int direccion;
     /**
      * Act - do whatever the Chell wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
-     public void act(){
-    if (Greenfoot.isKeyDown("UP")) {
-             movement(0);
+    public void act(){
+        if (Greenfoot.isKeyDown("UP")) {
+            movement(0);
         }
-        
+
         if (Greenfoot.isKeyDown("Down") ) {
-              movement(1);
+            movement(1);
         }
-    if (Greenfoot.isKeyDown("Left")) {
-           movement(2);
+        if (Greenfoot.isKeyDown("Left")) {
+            movement(2);
         }
-        
+
         if (Greenfoot.isKeyDown("Right") ) {
-             movement(3);
+            movement(3);
         }
-        
-}
-    public void movement(int direccion){
-    switch(direccion){
-        case 0:
-        setRotation(270);
-        setLocation(getX(),getY()-4);
-        if((isTouching(esquinaInferiorUniversalDerecha.class) || isTouching(esquinaInferiorUniversalIzquierda.class) || isTouching(esquinaSuperiorUniversalDerecha.class)||isTouching(esquinaSuperiorUniversalIzquierda.class)||isTouching(esquinainferiorDerecha.class)||isTouching(esquinainferiorizquierda.class)||isTouching(esquinasuperiorIzquierda.class)||isTouching(esquinasuperiorderecha.class)||isTouching(interseccionDerecha.class)||isTouching(interseccionInferior.class)||isTouching(interseccionIzquierda.class)||isTouching( esquinaInferiorUniversalIzquierda.class)||isTouching(interseccionSuperior.class)||isTouching(interseccionUniversal.class)||isTouching(paredHorizontal.class)||isTouching(paredHorizontalInferior.class)||isTouching(paredHorizontalSuperior.class)||isTouching(paredVertical.class)||isTouching(paredVerticalDerecha.class)||isTouching(paredVerticalIzquierda.class))){
-        setLocation(getX(),getY()+4);}
-        break;
-         case 1:
-        setRotation(90);
-        setLocation(getX(),getY()+4);
-        if((isTouching(esquinaInferiorUniversalDerecha.class) || isTouching(esquinaInferiorUniversalIzquierda.class)||isTouching(esquinaSuperiorUniversalDerecha.class)||isTouching(esquinaSuperiorUniversalIzquierda.class)||isTouching(esquinainferiorDerecha.class)||isTouching(esquinainferiorizquierda.class)||isTouching(esquinasuperiorIzquierda.class)||isTouching(esquinasuperiorderecha.class)||isTouching(interseccionDerecha.class)||isTouching(interseccionInferior.class)||isTouching(interseccionIzquierda.class)||isTouching( esquinaInferiorUniversalIzquierda.class)||isTouching(interseccionSuperior.class)||isTouching(interseccionUniversal.class)||isTouching(paredHorizontal.class)||isTouching(paredHorizontalInferior.class)||isTouching(paredHorizontalSuperior.class)||isTouching(paredVertical.class)||isTouching(paredVerticalDerecha.class)||isTouching(paredVerticalIzquierda.class) )){
-        setLocation(getX(),getY()-4);}
-        break;
-         case 2:
-        setRotation(0);
-        setLocation(getX()-4,getY());
-        if((isTouching(esquinaInferiorUniversalIzquierda.class)||isTouching(esquinaInferiorUniversalDerecha.class) ||isTouching(esquinaSuperiorUniversalDerecha.class)||isTouching(esquinaSuperiorUniversalIzquierda.class)||isTouching(esquinainferiorDerecha.class)||isTouching(esquinainferiorizquierda.class)||isTouching(esquinasuperiorIzquierda.class)||isTouching(esquinasuperiorderecha.class)||isTouching(interseccionDerecha.class)||isTouching(interseccionInferior.class)||isTouching(interseccionIzquierda.class)||isTouching( esquinaInferiorUniversalIzquierda.class)||isTouching(interseccionSuperior.class)||isTouching(interseccionUniversal.class)||isTouching(paredHorizontal.class)||isTouching(paredHorizontalInferior.class)||isTouching(paredHorizontalSuperior.class)||isTouching(paredVertical.class)||isTouching(paredVerticalDerecha.class)||isTouching(paredVerticalIzquierda.class) )){
-        setLocation(getX()+4,getY());}
-        if((isTouching(esquinaInferiorUniversalIzquierda.class)||isTouching(esquinaInferiorUniversalDerecha.class) ||isTouching(esquinaSuperiorUniversalDerecha.class)||isTouching(esquinaSuperiorUniversalIzquierda.class)||isTouching(esquinainferiorDerecha.class)||isTouching(esquinainferiorizquierda.class)||isTouching(esquinasuperiorIzquierda.class)||isTouching(esquinasuperiorderecha.class)||isTouching(interseccionDerecha.class)||isTouching(interseccionInferior.class)||isTouching(interseccionIzquierda.class)||isTouching( esquinaInferiorUniversalIzquierda.class)||isTouching(interseccionSuperior.class)||isTouching(interseccionUniversal.class)||isTouching(paredHorizontal.class)||isTouching(paredHorizontalInferior.class)||isTouching(paredHorizontalSuperior.class)||isTouching(paredVertical.class)||isTouching(paredVerticalDerecha.class)||isTouching(paredVerticalIzquierda.class))){
-        setLocation(getX()+4,getY());}
-        break;
-         case 3:
-        setRotation(0);
-        setLocation(getX()+4,getY());
-        if((isTouching(esquinaInferiorUniversalDerecha.class)||isTouching(esquinaInferiorUniversalIzquierda.class)||isTouching(esquinaSuperiorUniversalDerecha.class)||isTouching(esquinaSuperiorUniversalIzquierda.class)||isTouching(esquinainferiorDerecha.class)||isTouching(esquinainferiorizquierda.class)||isTouching(esquinasuperiorIzquierda.class)||isTouching(esquinasuperiorderecha.class)||isTouching(interseccionDerecha.class)||isTouching(interseccionInferior.class)||isTouching(interseccionIzquierda.class)||isTouching( esquinaInferiorUniversalIzquierda.class)||isTouching(interseccionSuperior.class)||isTouching(interseccionUniversal.class)||isTouching(paredHorizontal.class)||isTouching(paredHorizontalInferior.class)||isTouching(paredHorizontalSuperior.class)||isTouching(paredVertical.class)||isTouching(paredVerticalDerecha.class)||isTouching(paredVerticalIzquierda.class) )){
-        setLocation(getX()-4,getY());}
-        if((isTouching(esquinaInferiorUniversalDerecha.class)||isTouching(esquinaInferiorUniversalIzquierda.class)||isTouching(esquinaSuperiorUniversalDerecha.class)||isTouching(esquinaSuperiorUniversalIzquierda.class)||isTouching(esquinainferiorDerecha.class)||isTouching(esquinainferiorizquierda.class)||isTouching(esquinasuperiorIzquierda.class)||isTouching(esquinasuperiorderecha.class)||isTouching(interseccionDerecha.class)||isTouching(interseccionInferior.class)||isTouching(interseccionIzquierda.class)||isTouching( esquinaInferiorUniversalIzquierda.class)||isTouching(interseccionSuperior.class)||isTouching(interseccionUniversal.class)||isTouching(paredHorizontal.class)||isTouching(paredHorizontalInferior.class)||isTouching(paredHorizontalSuperior.class)||isTouching(paredVertical.class)||isTouching(paredVerticalDerecha.class)||isTouching(paredVerticalIzquierda.class) )){
-        setLocation(getX()-4,getY());}
-        break;
+
     }
-    
-    
+
+    public void movement(int direccion){
+        switch(direccion){
+            case 0:
+            setRotation(270);
+            setLocation(getX(),getY()-4);
+            if((isTouching(esquinaInferiorUniversalDerecha.class) || isTouching(Bloque.class) || isTouching(esquinaInferiorUniversalIzquierda.class) || isTouching(esquinaSuperiorUniversalDerecha.class)||isTouching(esquinaSuperiorUniversalIzquierda.class)||isTouching(esquinainferiorDerecha.class)||isTouching(esquinainferiorizquierda.class)||isTouching(esquinasuperiorIzquierda.class)||isTouching(esquinasuperiorderecha.class)||isTouching(interseccionDerecha.class)||isTouching(interseccionInferior.class)||isTouching(interseccionIzquierda.class)||isTouching( esquinaInferiorUniversalIzquierda.class)||isTouching(interseccionSuperior.class)||isTouching(interseccionUniversal.class)||isTouching(paredHorizontal.class)||isTouching(paredHorizontalInferior.class)||isTouching(paredHorizontalSuperior.class)||isTouching(paredVertical.class)||isTouching(paredVerticalDerecha.class)||isTouching(paredVerticalIzquierda.class))){
+                setLocation(getX(),getY()+4);}
+            break;
+            case 1:
+            setRotation(90);
+            setLocation(getX(),getY()+4);
+            if((isTouching(esquinaInferiorUniversalDerecha.class) ||isTouching(Bloque.class) || isTouching(esquinaInferiorUniversalIzquierda.class)||isTouching(esquinaSuperiorUniversalDerecha.class)||isTouching(esquinaSuperiorUniversalIzquierda.class)||isTouching(esquinainferiorDerecha.class)||isTouching(esquinainferiorizquierda.class)||isTouching(esquinasuperiorIzquierda.class)||isTouching(esquinasuperiorderecha.class)||isTouching(interseccionDerecha.class)||isTouching(interseccionInferior.class)||isTouching(interseccionIzquierda.class)||isTouching( esquinaInferiorUniversalIzquierda.class)||isTouching(interseccionSuperior.class)||isTouching(interseccionUniversal.class)||isTouching(paredHorizontal.class)||isTouching(paredHorizontalInferior.class)||isTouching(paredHorizontalSuperior.class)||isTouching(paredVertical.class)||isTouching(paredVerticalDerecha.class)||isTouching(paredVerticalIzquierda.class) )){
+                setLocation(getX(),getY()-4);}
+            break;
+            case 2:
+            setRotation(0);
+            setLocation(getX()-4,getY());
+            if((isTouching(esquinaInferiorUniversalIzquierda.class)||isTouching (Bloque.class) || isTouching(esquinaInferiorUniversalDerecha.class) ||isTouching(esquinaSuperiorUniversalDerecha.class)||isTouching(esquinaSuperiorUniversalIzquierda.class)||isTouching(esquinainferiorDerecha.class)||isTouching(esquinainferiorizquierda.class)||isTouching(esquinasuperiorIzquierda.class)||isTouching(esquinasuperiorderecha.class)||isTouching(interseccionDerecha.class)||isTouching(interseccionInferior.class)||isTouching(interseccionIzquierda.class)||isTouching( esquinaInferiorUniversalIzquierda.class)||isTouching(interseccionSuperior.class)||isTouching(interseccionUniversal.class)||isTouching(paredHorizontal.class)||isTouching(paredHorizontalInferior.class)||isTouching(paredHorizontalSuperior.class)||isTouching(paredVertical.class)||isTouching(paredVerticalDerecha.class)||isTouching(paredVerticalIzquierda.class) )){
+                setLocation(getX()+4,getY());}
+            if((isTouching(esquinaInferiorUniversalIzquierda.class)||isTouching(Bloque.class) || isTouching(esquinaInferiorUniversalDerecha.class) ||isTouching(esquinaSuperiorUniversalDerecha.class)||isTouching(esquinaSuperiorUniversalIzquierda.class)||isTouching(esquinainferiorDerecha.class)||isTouching(esquinainferiorizquierda.class)||isTouching(esquinasuperiorIzquierda.class)||isTouching(esquinasuperiorderecha.class)||isTouching(interseccionDerecha.class)||isTouching(interseccionInferior.class)||isTouching(interseccionIzquierda.class)||isTouching( esquinaInferiorUniversalIzquierda.class)||isTouching(interseccionSuperior.class)||isTouching(interseccionUniversal.class)||isTouching(paredHorizontal.class)||isTouching(paredHorizontalInferior.class)||isTouching(paredHorizontalSuperior.class)||isTouching(paredVertical.class)||isTouching(paredVerticalDerecha.class)||isTouching(paredVerticalIzquierda.class))){
+                setLocation(getX()+4,getY());}
+            break;
+            case 3:
+            setRotation(0);
+            setLocation(getX()+4,getY());
+            if((isTouching(esquinaInferiorUniversalDerecha.class) || isTouching(Bloque.class) || isTouching(esquinaInferiorUniversalIzquierda.class)||isTouching(esquinaSuperiorUniversalDerecha.class)||isTouching(esquinaSuperiorUniversalIzquierda.class)||isTouching(esquinainferiorDerecha.class)||isTouching(esquinainferiorizquierda.class)||isTouching(esquinasuperiorIzquierda.class)||isTouching(esquinasuperiorderecha.class)||isTouching(interseccionDerecha.class)||isTouching(interseccionInferior.class)||isTouching(interseccionIzquierda.class)||isTouching( esquinaInferiorUniversalIzquierda.class)||isTouching(interseccionSuperior.class)||isTouching(interseccionUniversal.class)||isTouching(paredHorizontal.class)||isTouching(paredHorizontalInferior.class)||isTouching(paredHorizontalSuperior.class)||isTouching(paredVertical.class)||isTouching(paredVerticalDerecha.class)||isTouching(paredVerticalIzquierda.class) )){
+                setLocation(getX()-4,getY());}
+            if((isTouching(esquinaInferiorUniversalDerecha.class) || isTouching(Bloque.class) || isTouching(esquinaInferiorUniversalIzquierda.class)||isTouching(esquinaSuperiorUniversalDerecha.class)||isTouching(esquinaSuperiorUniversalIzquierda.class)||isTouching(esquinainferiorDerecha.class)||isTouching(esquinainferiorizquierda.class)||isTouching(esquinasuperiorIzquierda.class)||isTouching(esquinasuperiorderecha.class)||isTouching(interseccionDerecha.class)||isTouching(interseccionInferior.class)||isTouching(interseccionIzquierda.class)||isTouching( esquinaInferiorUniversalIzquierda.class)||isTouching(interseccionSuperior.class)||isTouching(interseccionUniversal.class)||isTouching(paredHorizontal.class)||isTouching(paredHorizontalInferior.class)||isTouching(paredHorizontalSuperior.class)||isTouching(paredVertical.class)||isTouching(paredVerticalDerecha.class)||isTouching(paredVerticalIzquierda.class) )){
+                setLocation(getX()-4,getY());}
+            break;
+        }
+
     }
 }
--- Maze-is-a-lie/Maze1.java
+++ Maze-is-a-lie/Maze1.java
@@ -8,106 +8,108 @@ import java.io.*;
  * @version (a version number or a date)
  */ 
 
-    public class Maze1 extends World
-    {
-        public static GreenfootSound theme =  new  GreenfootSound("copiedCityNier.mp3");
-        private Chell chell =  new  Chell();
-        private portalAzul portal =  new  portalAzul();
-        private int direccion;
-        private Stopwatch uhr = new Stopwatch();
-        private static int timer = 0;
-        private static int timer2 =0;
-        
-        /**
-         * Constructor for objects of class Maze1.
-         * 
-         */
-        public Maze1()
-        {    
-            // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-            super(770, 630, 1);
-            createMaze();
-            addObject(chell, 385, 315);
-            addObject(uhr, 50, 50);
-            addObject(portal, 300, 500);
-        }
-        
-        public void act(){
-            //createMaze();
-            theme.play();
-            timer++;
-            if(timer==60){
-                timer2++;
-                timer=0;
-            }
-            uhr.updateStopwatch(timer2);
-           
-        }
-        
-        /*
-        public void createMaze()
-        {   
-            
-            int punterox=0;
-            int punteroy=0;
-            
-            try(FileReader fileReader = new FileReader("Maze1.txt"))
-            {
-                    int caracterLeido = fileReader.read();
-                    while(caracterLeido != -1){
-                            switch(caracterLeido){
-                            case 1:
-                                addObject(new esquinasuperiorIzquierda(),punterox,punteroy);
-                                punterox+=35;
-                                break;
-                            
-                            case 2:
-                                addObject(new paredHorizontalSuperior(),punterox,punteroy);
-                                punterox+=35;
-                                break;
-                            
-                            case 3:
-                                addObject(new esquinasuperiorderecha(),punterox,punteroy);
-                                punterox=0;
-                                punteroy+=35;
-                                break;
-                        }
-                        caracterLeido = fileReader.read();
-                    }
-                    fileReader.close();
-                }catch(IOException ex){
-                    System.err.println("Error al leer el archivo");
-                    ex.printStackTrace();
-                }
-            
-        }
-        */
-       
-       public void createMaze(){
-           int punterox=0;
-           int punteroy=0;
+public class Maze1 extends World
+{
+    public static GreenfootSound theme =  new  GreenfootSound("copiedCityNier.mp3");
+    private Chell chell =  new  Chell();
+    private portalAzul portal =  new  portalAzul();
+    private int direccion;
+    private Stopwatch uhr = new Stopwatch();
+    private static int timer = 0;
+    private static int timer2 =0;
 
-               addObject(new esquinasuperiorIzquierda(),punterox,punteroy);
-               punterox+=35;
-               while(punterox<=735){
-                   addObject(new paredHorizontalSuperior(),punterox,punteroy);
-                   punterox+=35;
-               }
-               addObject(new esquinasuperiorderecha(),punterox,punteroy);
-               punterox=0;
-               punteroy=+35;
-               addObject(new paredVerticalIzquierda(),punterox,punteroy);
-               punterox+=35;
-               
-       }
-       public static int getTimer(){
+    /**
+     * Constructor for objects of class Maze1.
+     * 
+     */
+    public Maze1()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(770, 630, 1);
+        createMaze();
+        addObject(chell, 385, 315);
+        addObject(uhr, 50, 50);
+        addObject(portal, 300, 500);
+    }
+
+    public void act(){
+        //createMaze();
+        theme.play();
+        timer++;
+        if(timer==60){
+            timer2++;
+            timer=0;
+        }
+        uhr.updateStopwatch(timer2);
+
+    }
+
+    public void createMaze()
+    {   
+
+        int punterox=0;
+        int punteroy=0;
+
+        try(FileReader fileReader = new FileReader("Maze1.txt"))
+        {
+            char[] ayuda = new char[1];
+            
+            int caracterLeido = fileReader.read(ayuda);
+            while(caracterLeido != -1){
+                switch(ayuda[0]){
+                    case '1':
+                    addObject(new Bloque(),punterox,punteroy);
+                    punterox+=35;
+                    break;
+
+                    case '2':
+                    punterox+=35;
+                    break;
+
+                    case '3':
+                    addObject(new Bloque(),punterox,punteroy);
+                    punterox=0;
+                    punteroy+=35;
+                    break;
+                }
+                caracterLeido = fileReader.read(ayuda);
+            }
+            fileReader.close();
+        }catch(IOException ex){
+            System.err.println("Error al leer el archivo");
+            ex.printStackTrace();
+        }
+
+    }
+
+    /*
+    public void createMaze(){
+        int punterox=0;
+        int punteroy=0;
+
+        addObject(new esquinasuperiorIzquierda(),punterox,punteroy);
+        punterox+=35;
+        while(punterox<=735){
+            addObject(new paredHorizontalSuperior(),punterox,punteroy);
+            punterox+=35;
+        }
+        addObject(new esquinasuperiorderecha(),punterox,punteroy);
+        punterox=0;
+        punteroy=+35;
+        addObject(new paredVerticalIzquierda(),punterox,punteroy);
+        punterox+=35;
+
+    }
+    */
+
+    public static int getTimer(){
         return timer;//da error en la 2 corrida
-        }
-       public static int getTimer2(){
+    }
+
+    public static int getTimer2(){
         return timer2; //da error en la 2 corrida
-        }
-       
-           
     }
     
+
+}
    
